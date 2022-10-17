baefa7ef44e57e0c7171803679086211d8898110
imhungry99 <imhungry102938576@gmail.com>
2022-10-17 05:29:44 -0300

    4.0

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function abrirMenu() {
+    document.body.classList.remove('menufechado')
+    document.body.classList.add('menuaberto')
+}
+function fecharMenu() {
+    document.body.classList.remove('menuaberto')
+    document.body.classList.add('menufechado')
+}

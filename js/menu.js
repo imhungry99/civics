71816f6369e5a05d362e362cb60c6a5e330965a8
ob1 wan <68697981+imhungry99@users.noreply.github.com>
2023-05-23 09:59:48 -0400

    Delete menu.js

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-function abrirMenu() {
-    document.body.classList.remove('menufechado')
-    document.body.classList.add('menuaberto')
-}
-function fecharMenu() {
-    document.body.classList.remove('menuaberto')
-    document.body.classList.add('menufechado')
-}
